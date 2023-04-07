0288c25eabb122511666bc27d938d8461f4d8a2a
maikofelix47 <maikofelix89@gmail.com>
2023-04-07 15:38:53 +0300

    Created OrderDetails model to better handle orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderDetails
 
 # Register your models here.
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Order._meta.fields]
+    list_display = ('id','date_created')
+
+class OrderDetailsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in OrderDetails._meta.fields]
 
 admin.site.register(Order,OrderAdmin)
+admin.site.register(OrderDetails, OrderDetailsAdmin)
 
--- orders/models.py
+++ orders/models.py
@@ -6,8 +6,19 @@ from products.models import Product
 # Create your models here.
 
 class Order(models.Model):
-    product = models.ManyToManyField(Product)
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.id}"
+
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)])
     date_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'OrderDetails'
 
 
