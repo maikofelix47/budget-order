d178373c761830c3e72009fd2f1e9fc36a6fb95f
maikofelix47 <maikofelix89@gmail.com>
2023-04-08 21:50:00 +0300

    Added base css, home view and set staticfiles sir to static

--- budget_order/settings.py
+++ budget_order/settings.py
@@ -130,10 +130,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 MEDIA_ROOT = BASE_DIR / env('MEDIA_ROOT')
 MEDIA_URL = env('MEDIA_URL')
