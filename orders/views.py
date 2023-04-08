137d599c657e0cbc38c37ec4f372ace8aea748ab
maikofelix47 <maikofelix89@gmail.com>
2023-04-08 20:43:53 +0300

    Added total order cost

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.db.models import Sum
 from .models import Order,OrderDetails
 
 # Create your views here.
@@ -11,8 +11,17 @@ class OrdersView(ListView):
 
 def order_details(request,pk):
     order_detail = OrderDetails.objects.filter(order_id=pk)
-    print('order_details', order_detail)
+    order_total = order_detail.raw("""SELECT 
+            SUM(od.quantity * p.price) as total_cost, 
+            od.order_id as id
+            FROM
+                orders_orderdetails od
+                join products_product p on (od.product_id = p.id)
+            WHERE od.order_id =  %s
+    group by od.order_id;
+    """,[pk])
     return render(request,'orders/order-details.html',{
-        'order_details': order_detail
+        'order_details': order_detail,
+        'order_total': order_total[0]
     })
 
