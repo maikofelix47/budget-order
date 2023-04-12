9c4d33581f4831f86514f995cdc93106f68af7fa
maikofelix47 <maikofelix89@gmail.com>
2023-04-12 19:05:01 +0300

    Added Rider to order

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
 
 from products.models import Product
+from riders.models import Rider
 
 # Create your models here.
 
@@ -15,6 +16,7 @@ class Order(models.Model):
 class OrderDetails(models.Model):
     order = models.ForeignKey(Order,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rider = models.ForeignKey(Rider,on_delete=models.CASCADE)
     quantity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)])
     date_created = models.DateTimeField(auto_now=True)
 
