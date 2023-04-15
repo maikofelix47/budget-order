5eae518b8165b016befad2dbb93e2415a006c962
maikofelix47 <maikofelix89@gmail.com>
2023-04-15 12:20:30 +0300

    Made order details view modifications to accurately capture order total

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
-from django.db.models import Sum
 from .models import Order,OrderDetails
 from .forms import OrderForm, OrderDetailsForm
 from dispatch.models import Dispatch
+from django.db import connection
+from django.http import HttpResponseBadRequest
 
 # Create your views here.
 class OrdersView(ListView):
@@ -14,29 +15,36 @@ class OrdersView(ListView):
 
 def order_details(request,pk):
     order_detail = OrderDetails.objects.filter(order_id=pk)
-    dispatch_details = Dispatch.objects.filter(order_id = pk)
+    dispatch_details = Dispatch.objects.filter(order_id=pk)
+    order_total = []
+    order_cost = 0
+
     try:
-        order_total = order_detail.raw("""SELECT 
+        order_total = get_order_total(pk)
+        order_cost = order_total[0]
+    except:
+       return HttpResponseBadRequest('There is an issue with the specified order')
+    
+    return render(request,'orders/order-details.html',{
+        'order_details': order_detail,
+        'order_total': order_cost,
+        'order_id': pk,
+        'dispatch_details': dispatch_details
+    })
+
+def get_order_total(orderId):
+    with connection.cursor() as cursor:
+        cursor.execute(""" 
+            SELECT 
                 SUM(od.quantity * p.price) as total_cost, 
                 od.order_id as id
                 FROM
                     orders_orderdetails od
                     join products_product p on (od.product_id = p.id)
                 WHERE od.order_id =  %s
-        group by od.order_id;
-        """,[pk])
-        order_cost = order_total[0]
-        dispatch_data = dispatch_details[0]
-    except:
-        order_cost = 0
-        dispatch_data = []
-    
-    return render(request,'orders/order-details.html',{
-        'order_details': order_detail,
-        'order_total': order_cost,
-        'order_id': pk,
-        'dispatch_details': dispatch_data
-    })
+        group by od.order_id""",[orderId])
+        row = cursor.fetchone()
+    return row
 
 class CreateOrderView(CreateView):
     form_class = OrderForm
